7fb40e3369fa7b6a311f7c0d4f972c20172c466b
Daniel Leonardo <daniel.fnz@hotmail.com>
2019-02-02 11:41:45 -0200

    Arquivos iniciais do projeto

---
+++ app/src/main/java/danielfnz/com/br/ecommerce/Ecommerce.java
@@ -0,0 +1,13 @@
+package danielfnz.com.br.ecommerce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ecommerce extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecommerce);
+    }
+}
